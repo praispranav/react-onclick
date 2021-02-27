7a5f9190389e5f6e3344966a877c6a977c944542
praispranav <praispranav@gmail.com>
2021-02-27 13:16:56 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import "./styles.css";
+// import "./Desert.jpg";
+import React from "react";
 
-export default function App() {
-  return (
-    <div className="App">
-      <button onMouseOver={myFunction}>Click me</button>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 2
+      };
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        {/* <img
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"
+          onMouseOver={myFunction}
+        /> */}
+        <p>{this.state.count}</p>
+        <br />
+
+        <button onClick={this.handleClick}>Click me</button>
+      </div>
+    );
+  }
 }
-function myFunction() {
-  console.log("Hello");
-}
+
+export default App;
